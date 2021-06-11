3275b4db165379b1e0b62d4aeffa50aa2a6888d6
Adityakane2001 <adityakane1@gmail.com>
2021-06-11 10:01:22 +0000

    Added docstrings, randaugment function

--- dataset/imagenet.py
+++ dataset/imagenet.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 import os
+from official.vision.image_classification.augment import RandAugment
 
 
 class ImageNet:
-    """Class for all ImageNet related functions, includes TFRecords loading, 
-    preprocessing and augmentations. TFRecords must follow the format given 
+    """Class for all ImageNet related functions, includes TFRecords loading,
+    preprocessing and augmentations. TFRecords must follow the format given
     below.
-    
+
     Args:
-        tfrecs_filepath: list of filepaths of all TFRecords files 
+        tfrecs_filepath: list of filepaths of all TFRecords files
         batch_size: batch_size for the Dataset
         image_size: final image size of the images in the dataset
         augment_fn: function to apply to dataset after loading raw TFrecords
@@ -16,15 +17,15 @@ class ImageNet:
                     - Random sized crop (train only)
                     - Scale and center crop
                     - RandAugment (TODO)
+        num_classes: number of classes
+        randaugment: True of RandAugment is to be applied
 
     Class attributes:
         TFRECS_FORMAT: Expected format of TFRecords stored.
         _MEAN: Channel wise mean for ImageNet
         _STD: Channel wise standard deviation for ImageNet
-          
-    """
 
-    #TODO: Format clipper function
+    """
 
     TFRECS_FORMAT = {
         "image": tf.io.FixedLenFeature([], tf.string),
@@ -38,13 +39,14 @@ class ImageNet:
     _MEAN = tf.constant([0.485, 0.456, 0.406])
     _STD = tf.constant([0.229, 0.224, 0.225])
 
-    
     def __init__(
         self,
         tfrecs_filepath=None,
         batch_size=128,
         image_size=224,
         augment_fn="default",
+        num_classes=10,
+        randaugment=True,
     ):
 
         if tfrecs_filepath is None:
@@ -53,16 +55,20 @@ class ImageNet:
         self.batch_size = batch_size
         self.image_size = image_size
         self.augment_fn = augment_fn
+        self.num_classes = num_classes
+        self.randaugment = randaugment
+        if self.randaugment:
+            self.augmenter = RandAugment(magnitude=5)
 
     def decode_example(self, example):
         """Decodes an example to its individual attributes
-        
+
         Args:
             example: A TFRecord dataset example.
 
         Returns:
             Dict containing attributes from a single example. Follows
-            the same names as TFRECORDS_FORMAT.     
+            the same names as TFRECORDS_FORMAT.
         """
         image = tf.cast(tf.io.decode_jpeg(example["image"]), tf.float32)
         height = example["height"]
@@ -71,17 +77,20 @@ class ImageNet:
         label = example["label"]
         synset = example["synset"]
         return {
-                "image": image,
-                "height": height,
-                "width": width,
-                "filename": filename,
-                "label": label,
-                "synset": synset
-            }
-    
-    def _read_tfrecs(self):
-        """Function for reading and loading TFRecords into a tf.data.Dataset."""
+            "image": image,
+            "height": height,
+            "width": width,
+            "filename": filename,
+            "label": label,
+            "synset": synset,
+        }
 
+    def _read_tfrecs(self):
+        """Function for reading and loading TFRecords into a tf.data.Dataset.
+
+        Returns:
+            A tf.data.Dataset
+        """
         ds = tf.data.TFRecordDataset(self.tfrecs_filepath)
         ds = ds.map(
             lambda example: tf.io.parse_example(example, ImageNet.TFRECS_FORMAT)
@@ -90,9 +99,9 @@ class ImageNet:
         return ds
 
     def _scale_and_center_crop(self, image, h, w, scale_size, final_size):
-        """Resizes image to given scale size and returns a center crop. Aspect 
-        ratio is NOT maintained. 
-        
+        """Resizes image to given scale size and returns a center crop. Aspect
+        ratio is NOT maintained.
+
         Args:
             image: tensor of the image
             h: initial height of image
@@ -115,24 +124,24 @@ class ImageNet:
             im = tf.image.resize(image, (w, h))
         else:
             im = tf.image.resize(image, (w, h))
-        x = (w - final_size) / 2
-        y = (h - final_size) / 2
+        x = tf.cast((w - final_size) / 2, tf.int64)
+        y = tf.cast((h - final_size) / 2, tf.int64)
         return im[y : (y + final_size), x : (x + final_size), :]
 
-    def _random_sized_crop(self, example, min_area=0.08, max_iter=10):
+    def random_sized_crop(self, example, min_area=0.08, max_iter=10):
         """
-        Randomly chooses an area and aspect ratio and resizes the image to those 
+        Randomly chooses an area and aspect ratio and resizes the image to those
         values.
 
         Args:
             example: A dataset example.
             min_area: Minimum area of image to be used
-            max_iter: Maximum iteration to try before implementing scale and 
-                center crop. Randomly genereted width and height must be lower 
+            max_iter: Maximum iteration to try before implementing scale and
+                center crop. Randomly genereted width and height must be lower
                 than original width and height before max_iter.
-        
+
         Returns:
-            Tensor of shape (self.image_size, self.image_size, 3)        
+            Example of same format as TFRECS_FORMAT
         """
 
         h, w = tf.cast(example["height"], tf.int64), tf.cast(
@@ -187,6 +196,7 @@ class ImageNet:
                 "width": self.image_size,
                 "filename": example["filename"],
                 "label": example["label"],
+                "synset": example["synset"],
             }
 
         image = self._scale_and_center_crop(
@@ -198,9 +208,37 @@ class ImageNet:
             "width": self.image_size,
             "filename": example["filename"],
             "label": example["label"],
+            "synset": example["synset"],
         }
 
-    
+    def clip_example(self, example):
+        """Takes an example having keys 'image' and 'label' and returns example
+        with keys 'image' and 'target'. 'target' is one hot encoded.
+
+        Args:
+            example: an example having keys 'image' and 'label'
+
+        Returns:
+            example having keys 'image' and 'target'
+        """
+        return {
+            "image": example["image"],
+            "target": tf.one_hot(example["label"], self.num_classes),
+        }
+
+    def _randaugment(self, example):
+        """Wrapper for tf vision's RandAugment.distort function which
+        accepts examples as input instead of images. Uses magnitude = 5
+        as per pycls/pycls/datasets/augment.py#L29.
+
+        Args:
+            example: Example having the key 'image'
+
+        Returns:
+            example in which RandAugment has been applied to the image
+        """
+        example['image'] = self.augmenter.distort(example['image'])
+        return example
 
     def make_dataset(
         self,
@@ -211,13 +249,20 @@ class ImageNet:
 
         Returns:
             Dataset having the final format as follows:
-            TODO: final format         
+            {
+                'image' : (self.image_size, self.image_size, 3)
+                'target' : (num_classes,)
+            }
         """
         ds = self._read_tfrecs()
 
         if self.augment_fn == "default":
-            ds = ds.map(lambda example: self._random_sized_crop(example))
+            ds = ds.map(lambda example: self.random_sized_crop(example))
+            if self.randaugment:
+                ds = ds.map(lambda example: self._randaugment(example))
+            #ds = ds.map(lambda example: self.clip_example(example))
 
         else:
             ds = ds.map(lambda example: self.augment_fn(example))
+
         return ds
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 import math
 from utils.tfrecords_utils import *
 from dataset.imagenet import ImageNet
-
+import matplotlib.pyplot as plt
 
 def main():
     parser = argparse.ArgumentParser(description="Make TFRecords")
@@ -17,18 +17,21 @@ def main():
     parser.add_argument("--shards", type=int, default=10)
 
     args = parser.parse_args()
-    make_tfrecs(
-        dataset_base_dir=args.data_dir,
-        output_dir=args.odir,
-        file_prefix=args.file_prefix,
-        synset_filepath=args.synset_filepath,
-        num_shards=args.shards,
-    )
-    # imgnet = ImageNet([os.path.join('/content',i) for i in os.listdir('/content') if i.startswith('trial4')])
-    # ds = imgnet.make_dataset()
-    # for i in ds:
-    #   print(i)
-    #   break
+    # make_tfrecs(
+    #     dataset_base_dir=args.data_dir,
+    #     output_dir=args.odir,
+    #     file_prefix=args.file_prefix,
+    #     synset_filepath=args.synset_filepath,
+    #     num_shards=args.shards,
+    # )
+    imgnet = ImageNet([os.path.join('/content',i) for i in os.listdir('/content') if i.startswith('trial4')])
+    ds = imgnet.make_dataset()
+    for i in ds:
+      print(i)
+      im = i['image']/255.
+      plt.imshow(im)
+      plt.savefig('image.jpeg')
+      break
 
 
 main()
