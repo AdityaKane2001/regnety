583ffd8f0e14432f0aa8e9491c24a76a11c0bb84
Aditya Kane <adityakane1@gmail.com>
2021-07-08 16:37:34 +0530

    Requested changes

--- regnety/dataset/imagenet.py
+++ regnety/dataset/imagenet.py
@@ -5,6 +5,8 @@ import os
 
 from typing import Union, Callable, Tuple, List, Type
 
+AUTO = tf.data.AUTOTUNE
+
 _TFRECS_FORMAT = {
         "image": tf.io.FixedLenFeature([], tf.string),
         "height": tf.io.FixedLenFeature([], tf.int64),
@@ -18,20 +20,25 @@ _TFRECS_FORMAT = {
 class ImageNet:
     """Class for all ImageNet data-related functions, including TFRecord 
     parsing along with augmentation transforms. TFRecords must follow the format
-    given below. If not specified otherwise in `augment_fn` argument, following 
+    given in _TFRECS_FORMAT. If not specified otherwise in `augment_fn` argument, following 
     augmentations are applied to the dataset:
-    - Random sized crop (train only)
-    - Scale and center crop (validation and test only)
+    - Color Jitter (random brightness, hue, saturation, contrast, flip)
+        This augmentation is inspired by SimCLR (https://arxiv.org/abs/2002.05709). 
+        The strength parameter is set to 5, which controlsthe effect of augmentations.
+    - Random rotate
+    - Random crop and resize
+
+    If `augment_fn` argument is not set to the string "default", it should be set to 
+    a callable object. That callable must take exactly two arguments: `image` and `target`
+    and must return two values corresponding to the same. 
+
     Args:
         tfrecs_filepath: list of filepaths of all TFRecords files
         batch_size: batch_size for the Dataset
         image_size: final image size of the images in the dataset
         augment_fn: function to apply to dataset after loading raw TFrecords
         num_classes: number of classes
-        randaugment: True if RandAugment is to be applied after other 
-            preprocessing functions. It is applied even if this is True and 
-            augment_fn is not 'default'. It is not applied in any case if this
-            argument is False.
+        
     """
     def __init__(
         self,
@@ -39,33 +46,40 @@ class ImageNet:
         batch_size: int = 1024,
         image_size: int = 512,
         augment_fn: Union[str, Callable]  = "default",
-        num_classes: int = 10,
-        randaugment: bool = True,
+        num_classes: int = 1000,
     ):
 
         if (tfrecs_filepath is None) or  (tfrecs_filepath == []):
             raise ValueError("List of TFrecords paths cannot be None or empty")
+
+        if self.augment_fn == 'default':
+            self.default_augment = True
+            self.strength = 5
+        else:
+            self.default_augment = False
+            self.strength = -1
+        
         self.tfrecs_filepath = tfrecs_filepath
         self.batch_size = batch_size
-        self.image_size = tf.constant([image_size] * self.batch_size) 
+        self.image_size = image_size
         self.augment_fn = augment_fn
         self.num_classes = num_classes
-        self.randaugment = randaugment
-        if self.randaugment:
-            self.strength = 5
 
     
     def decode_example(self, example_: tf.Tensor) -> dict:
-        """Decodes an example to its individual attributes
+        """Decodes an example to its individual attributes.
+
         Args:
             example: A TFRecord dataset example.
+
         Returns:
             Dict containing attributes from a single example. Follows
-            the same names as TFRECORDS_FORMAT.
+            the same names as _TFRECS_FORMAT.
         """
 
         example =  tf.io.parse_example(example_, _TFRECS_FORMAT)
-        image = tf.reshape(tf.io.decode_jpeg(example["image"]), (512,512,3))
+        image = tf.reshape(tf.io.decode_jpeg(
+            example["image"]), (self.image_size, self.image_size, 3))
         height = example['height']
         width = example['width']
         filename = example["filename"]
@@ -83,41 +97,40 @@ class ImageNet:
 
     def _read_tfrecs(self) -> Type[tf.data.Dataset]:
         """Function for reading and loading TFRecords into a tf.data.Dataset.
+
+        Args: None.
+
         Returns:
-            A tf.data.Dataset
+            A tf.data.Dataset instance.
         """
 
         files = tf.data.Dataset.list_files(self.tfrecs_filepath)
-
-        options = tf.data.Options()
-        options.experimental_deterministic = False
-
-        files = files.with_options(options)
-
         ds = files.interleave(tf.data.TFRecordDataset, 
-          num_parallel_calls = tf.data.AUTOTUNE,
+          num_parallel_calls = AUTO,
           deterministic=False)
 
         ds = ds.map(
             self.decode_example, 
-            num_parallel_calls = tf.data.AUTOTUNE 
+            num_parallel_calls = AUTO 
         )
 
         ds = ds.cache('tf_cache')
        
         ds = ds.repeat()
         ds = ds.batch(self.batch_size)
-        ds = ds.prefetch(tf.data.AUTOTUNE)
+        ds = ds.prefetch(AUTO)
         return ds
 
   
     def _one_hot_encode_example(self, example: dict) -> tuple:
         """Takes an example having keys 'image' and 'label' and returns example
         with keys 'image' and 'target'. 'target' is one hot encoded.
+
         Args:
-            example: an example having keys 'image' and 'label'
+            example: an example dict having keys 'image' and 'label'.
+
         Returns:
-            example having keys 'image' and 'target'
+            Tuple having structure (image_tensor, targets_tensor).
         """
         return (example["image"], tf.one_hot(example["label"], self.num_classes))
 
@@ -125,14 +138,15 @@ class ImageNet:
 
     def color_jitter(self, image: tf.Tensor, target: tf.Tensor) -> tuple:
         """
-        Performs color jitter on the batch. Random brightness, hue, saturation, 
-        and contrast
+        Performs color jitter on the batch. It performs random brightness, hue, saturation, 
+        contrast and random left-right flip. 
 
         Args: 
-            example:example with batch of images to be augmented
+            image: Batch of images to perform color jitter on.
+            target: Target tensor.
 
         Returns:
-            Augmented example with batch of images with same dimensions 
+            Augmented example with batch of images and targets with same dimensions.
         """
         
         
@@ -157,13 +171,14 @@ class ImageNet:
 
     def random_rotate(self, image: tf.Tensor, target: tf.Tensor) -> tuple:
         """"
-        Randomly rotates images
+        Returns randomly rotated batch of images.
 
         Args: 
-            example:example with batch of images to be augmented
+            image: Batch of images to perform random rotation on.
+            target: Target tensor.
 
         Returns:
-            Augmented example with batch of images with same dimensions 
+            Augmented example with batch of images and targets with same dimensions.
         """
 
         angles = tf.random.uniform((self.batch_size,)) * (math.pi / 2.)
@@ -173,51 +188,62 @@ class ImageNet:
 
     def random_crop(self, image: tf.Tensor, target: tf.Tensor) -> tuple:
         """"
-        Returns random crop of images
+        Returns random crops of images. 
 
         Args: 
-            example:example with batch of images to be augmented
+            image: Batch of images to perform random crop on.
+            target: Target tensor.
 
         Returns:
-            Cropped example with batch of images with same dimensions 
+            Augmented example with batch of images and targets with same dimensions.
         """
 
-        cropped = tf.image.random_crop(image, size=(self.batch_size, 320, 320, 3))
+        cropped = tf.image.random_crop(image, size = (self.batch_size, 320, 320, 3))
         return cropped, target        
 
 
     def make_dataset(self) -> Type[tf.data.Dataset]:
         """
         Function to apply all preprocessing and augmentations on dataset using
-        dataset.map().
+        tf.data.dataset.map().
+
+        If `augment_fn` argument is not set to the string "default", it should be set to 
+        a callable object. That callable must take exactly two arguments: `image` and `target`
+        and must return two values corresponding to the same. 
+
+        Args: None.
+
         Returns:
-            Dataset having the final format as follows:
-            {
-                'image' : (batch_size, self.image_size, self.image_size, 3)
-                'target' : (batch_size, num_classes)
-            }
+            tf.data.Dataset instance having the final format as follows:
+            (image, target)
         """
         ds = self._read_tfrecs()
         
         ds = ds.map(
             self._one_hot_encode_example,
-            num_parallel_calls = tf.data.AUTOTUNE
+            num_parallel_calls = AUTO
         )
 
-        if self.randaugment:
+        if self.default_augment:
             ds = ds.map(
                 self.color_jitter,
-                num_parallel_calls = tf.data.AUTOTUNE
+                num_parallel_calls = AUTO
             )
 
             ds = ds.map(
                 self.random_rotate,
-                num_parallel_calls = tf.data.AUTOTUNE
+                num_parallel_calls = AUTO
             )
 
             ds =  ds.map(
                 self.random_crop,
-                num_parallel_calls = tf.data.AUTOTUNE
+                num_parallel_calls = AUTO
+            )
+        
+        else:
+            ds = ds.map(
+                self.augment_fn,
+                num_parallel_calls = AUTO
             )
 
 
