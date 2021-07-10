0f4f46e987f2976a5061473eedb8115b2d248508
Adityakane2001 <adityakane1@gmail.com>
2021-07-10 10:22:12 +0000

    Added aux tf functions

--- regnety/utils/beam_utils.py
+++ regnety/utils/beam_utils.py
@@ -4,6 +4,26 @@ import tensorflow as tf
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
+
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = (
+            value.numpy()
+        )  # BytesList won't unpack a string from an EagerTensor.
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
 def create_collection(list1, list2, list3):
 
     final_list = [
@@ -18,7 +38,7 @@ def create_collection(list1, list2, list3):
 
     return coll
 
-    
+
 class MakeImageDoFn(beam.DoFn):
     def process(
         self, 
