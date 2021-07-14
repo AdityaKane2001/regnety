17bfd9f5b2247bb7478d1a13b834268d607b80d6
Aditya Kane <adityakane1@gmail.com>
2021-07-14 22:43:16 +0530

    Kaggle compat

--- regnety/utils/train_utils.py
+++ regnety/utils/train_utils.py
@@ -79,14 +79,20 @@ def get_callbacks(cfg):
     ]
 
 
-def connect_to_tpu(tpu_address):
-    cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-        tpu=tpu_address)
-    if tpu_address not in ("", "local"):
-        tf.config.experimental_connect_to_cluster(cluster_resolver)
-    tf.distribute.TPUStrategy(cluster_resolver)
-    print('Running on TPU ', cluster_resolver.master())
-    tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
-    strategy = tf.distribute.experimental.TPUStrategy(cluster_resolver)
-    print("REPLICAS: ", strategy.num_replicas_in_sync)
-    return cluster_resolver, strategy
+def connect_to_tpu(tpu_address: str = None):
+    if tpu_address is not None:
+        cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
+            tpu=tpu_address)
+        if tpu_address not in ("", "local"):
+            tf.config.experimental_connect_to_cluster(cluster_resolver)
+        tf.distribute.TPUStrategy(cluster_resolver)
+        print('Running on TPU ', cluster_resolver.master())
+        tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
+        strategy = tf.distribute.experimental.TPUStrategy(cluster_resolver)
+        print("REPLICAS: ", strategy.num_replicas_in_sync)
+        return cluster_resolver, strategy
+    else:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
+        # instantiate a distribution strategy
+        tpu_strategy = tf.distribute.experimental.TPUStrategy(tpu)
+        return tpu, tpu_strategy
